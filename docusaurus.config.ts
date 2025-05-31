eb09cb0a9a127dc81ea9f3d56be2f3c2ee3ce0f4
ChatSites™ <info@chatsites.io>
2025-05-31 22:18:12 +0000

    bug fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -4,26 +4,15 @@ import type * as Preset from '@docusaurus/preset-classic';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
-const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+export default {
+  title: 'The Human Channel',
+  url: 'https://the-human-channel.github.io',
+  baseUrl: '/the-human-channel-site/',
+  organizationName: 'the-human-channel',
+  projectName: 'the-human-channel-site',
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
-  future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
-  },
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -145,4 +134,3 @@ const config: Config = {
   } satisfies Preset.ThemeConfig,
 };
 
-export default config;
