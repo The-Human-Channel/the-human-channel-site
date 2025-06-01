546632faaee28db5c707b71ff58c44839e53ef9f
ChatSites™ <info@chatsites.io>
2025-06-01 00:38:17 +0000

    bugs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -42,7 +42,7 @@ const config: Config = {
   title: 'The Human Channel',
   logo: {
     alt: 'The Human Channel Logo',
-    src: '/logo.svg',
+    src: 'img/logo.svg',
   },
   items: [
     {to: '/blog', label: 'Blog', position: 'left'},
