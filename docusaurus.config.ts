262cc25d8c131a285c38a7c0a5c210cb75316bab
ChatSites™ <info@chatsites.io>
2025-06-01 00:42:25 +0000

    bugs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -37,37 +37,36 @@ const config: Config = {
   ],
 
   themeConfig: {
-    image: 'img/docusaurus-social-card.jpg',
-    navbar: {
-  title: 'The Human Channel',
-  logo: {
-    alt: 'The Human Channel Logo',
-    src: 'img/logo.svg',
+  image: 'img/docusaurus-social-card.jpg',
+  navbar: {
+    title: 'The Human Channel',
+    logo: {
+      alt: 'The Human Channel Logo',
+      src: 'img/logo.svg',
+    },
+    items: [
+      { to: '/blog', label: 'Blog', position: 'left' },
+      { to: '/docs/intro', label: 'Whitepapers', position: 'left' },
+      {
+        href: 'https://github.com/the-human-channel/the-human-channel-site',
+        label: 'GitHub',
+        position: 'right',
+      },
+    ],
   },
-  items: [
-    {to: '/blog', label: 'Blog', position: 'left'},
-    {to: '/docs/intro', label: 'Whitepapers', position: 'left'},
-    {
-      href: 'https://github.com/the-human-channel/the-human-channel-site',
-      label: 'GitHub',
-      position: 'right',
-    },
-  ],
-},
+  footer: {
+    style: 'dark',
+    links: [
+      { title: 'Docs', items: [{ label: 'Whitepapers', to: '/docs/intro' }] },
+      { title: 'More', items: [{ label: 'Blog', to: '/blog' }] }
+    ],
+    copyright: `Copyright © ${new Date().getFullYear()} The Human Channel.`
+  },
+  prism: {
+    theme: prismThemes.github,
+    darkTheme: prismThemes.dracula
+  }
+} satisfies Preset.ThemeConfig
 
-    footer: {
-      style: 'dark',
-      links: [
-        { title: 'Docs', items: [{ label: 'Whitepapers', to: '/docs/intro' }] },
-        { title: 'More', items: [{ label: 'Blog', to: '/blog' }] }
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} The Human Channel.`
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula
-    }
-  } satisfies Preset.ThemeConfig
-};
 
 export default config;
